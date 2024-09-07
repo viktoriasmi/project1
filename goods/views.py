a120ac391b18340a48ef1f15f595e02bf9702064
viktoriasmi <viktoria.smi@mail.ru>
2024-09-07 12:15:28 +0300

    pagination 2

--- goods/views.py
+++ goods/views.py
@@ -4,7 +4,9 @@ from django.template.defaulttags import comment
 
 from goods.models import Products
 
-def catalog(request, category_slug, page=1):
+def catalog(request, category_slug):
+
+    page = request.GET.get('page', 1)
     
     if category_slug == 'all':    
         goods = Products.objects.all()
@@ -13,7 +15,7 @@ def catalog(request, category_slug, page=1):
     
 
     paginator = Paginator(goods, 12)  
-    current_page = paginator.page(page)
+    current_page = paginator.page(int(page))
     
 
     context = {
