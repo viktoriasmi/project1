e572e9ef3e72d953721d377fce1aeb07edf65e5f
viktoriasmi <viktoria.smi@mail.ru>
2024-09-08 16:00:41 +0300

    filter pagi

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,3 +1,5 @@
+from urllib import request
+from urllib.parse import urlencode
 from django import template
 from goods.models import Categories
 
@@ -7,3 +9,8 @@ register = template.Library()
 def tag_categories():
     return Categories.objects.all()
 
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
