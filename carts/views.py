c43ae6261b2f78559a9fb64e5f52bb81a44e2ee0
viktoriasmi <viktoria.smi@mail.ru>
2024-10-15 12:25:53 +0300

    смена количества в корзине

--- carts/views.py
+++ carts/views.py
@@ -21,8 +21,25 @@ def cart_add(request, product_slug):
             Cart.objects.create(user=request.user, product=product, quantity=1)
     return redirect(request.META['HTTP_REFERER'])
 
-def cart_change(request, product_slug):
-    ...
+def cart_change(request, cart_id):
+    if request.method == 'POST':
+        action = request.POST.get('action')
 
-def cart_remove(request, product_slug):
-    ...
+        cart = Cart.objects.get(id=cart_id)
+
+        if action == 'increase':
+            cart.quantity += 1
+            cart.save()
+        elif action == 'decrease' and cart.quantity > 1:  
+            cart.quantity -= 1
+            cart.save()
+
+    return redirect(request.META['HTTP_REFERER'])
+
+
+
+def cart_remove(request, cart_id):
+    
+    cart = Cart.objects.get(id=cart_id)
+    cart.delete()
+    return redirect(request.META['HTTP_REFERER'])
